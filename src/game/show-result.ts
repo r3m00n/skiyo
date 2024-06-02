7f3d6bfc0fd89b224acd64f737865024b85e6b68
Merlin Förster <foerster.m@eppendorf.de>
2024-06-02 21:52:42 +0200

    👨‍🎨 upgade logs

--- src/game/show-result.ts
+++ src/game/show-result.ts
@@ -3,22 +3,33 @@ import { Result } from '@/types';
 export const showResult = (result: Result[]) => {
   const bot1Wins = result.filter((r) => r.points1 < r.points2).length;
   const bot2Wins = result.filter((r) => r.points2 < r.points1).length;
-  const bot1WinPercentage = (bot1Wins / result.length) * 100;
-  const bot2WinPercentage = (bot2Wins / result.length) * 100;
-  const bot1AveragePoints =
-    result.reduce((acc, r) => acc + r.points1, 0) / result.length;
-  const bot2AveragePoints =
-    result.reduce((acc, r) => acc + r.points2, 0) / result.length;
-  const averageTurns =
-    result.reduce((acc, r) => acc + r.turns, 0) / result.length;
+  const decidedGames = bot1Wins + bot2Wins;
+  const bot1WinPercentage = round((bot1Wins / decidedGames) * 100);
+  const bot2WinPercentage = round((bot2Wins / decidedGames) * 100);
+  const bot1AveragePoints = round(
+    result.reduce((acc, r) => acc + r.points1, 0) / result.length
+  );
+  const bot2AveragePoints = round(
+    result.reduce((acc, r) => acc + r.points2, 0) / result.length
+  );
+  const bot1MinPoints = result.reduce((acc, r) => Math.min(acc, r.points1), 0);
+  const bot2MinPoints = result.reduce((acc, r) => Math.min(acc, r.points2), 0);
+  const bot1MaxPoints = result.reduce((acc, r) => Math.max(acc, r.points1), 0);
+  const bot2MaxPoints = result.reduce((acc, r) => Math.max(acc, r.points2), 0);
+  const averageTurns = round(
+    result.reduce((acc, r) => acc + r.turns, 0) / result.length
+  );
 
   console.log(
     `Bot${bot1Wins > bot2Wins ? 1 : 2} wins ${bot1Wins > bot2Wins ? bot1WinPercentage : bot2WinPercentage}% of the games!`
   );
-  console.log(`Bot1 average: ${bot1AveragePoints}`);
-  console.log(`Bot2 average: ${bot2AveragePoints}`);
   console.log(
-    `Draws: ${result.length - bot1Wins - bot2Wins} (${((result.length - bot1Wins - bot2Wins) / result.length) * 100}%)`
+    `B1 average: ${bot1AveragePoints.toString().padEnd(6)}| Min: ${bot1MinPoints.toString().padEnd(3)} Max: ${bot1MaxPoints.toString().padEnd(3)}`
+  );
+  console.log(
+    `B2 average: ${bot2AveragePoints.toString().padEnd(6)}| Min: ${bot2MinPoints.toString().padEnd(3)} Max: ${bot2MaxPoints.toString().padEnd(3)}`
   );
   console.log(`Average Turns: ${averageTurns}`);
 };
+
+const round = (num: number): number => Math.round(num * 100) / 100;
