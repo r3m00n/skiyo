f88a11ddfd946c7ab640dcf20cfc7ecccece56ee
Merlin Förster <moin@merlin.hamburg>
2024-06-13 19:03:28 +0200

    add tests

--- test/bot/r3m00n.spec.ts
+++ test/bot/r3m00n.spec.ts
@@ -4,7 +4,7 @@ import { afterStartBoard, turn1Board1 } from './utils/boards';
 
 basicTests(r3m00nBot);
 
-describe('r3m00nBot', () => {
+describe('Game Start', () => {
   it('should flip second card in same column if first card is high', () => {
     const { action, location } = r3m00nBot(
       [
@@ -37,5 +37,40 @@ describe('r3m00nBot', () => {
     );
     expect(action).toBe('flip');
     expect(location![0]).not.toBe(0);
+  });
+});
+
+describe('Early Game', () => {
+  it('High card matching collecting card', () => {
+    const { action, location } = r3m00nBot(
+      [
+        [12, 12, undefined],
+        [undefined, undefined, undefined],
+        [undefined, undefined, undefined],
+        [undefined, undefined, undefined],
+      ],
+      afterStartBoard,
+      12,
+      undefined,
+      [0]
+    );
+    expect(action).toBe('takeFromDisgardPile');
+    expect(location!).toBe([0, 2]);
+  });
+
+  it('Not take any high card', () => {
+    const { action, location } = r3m00nBot(
+      [
+        [12, 12, undefined],
+        [undefined, undefined, undefined],
+        [undefined, undefined, undefined],
+        [undefined, undefined, undefined],
+      ],
+      afterStartBoard,
+      11,
+      undefined,
+      [0]
+    );
+    expect(action).toBe('draw');
   });
 });
