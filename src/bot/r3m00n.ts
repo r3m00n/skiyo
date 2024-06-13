5c3ce3aaf18fadd8a2ed5b3bb556372420612563
Merlin Förster <moin@merlin.hamburg>
2024-06-13 19:02:53 +0200

    enhance bot

--- src/bot/r3m00n.ts
+++ src/bot/r3m00n.ts
@@ -1,10 +1,9 @@
 import { Board, Card, DiscardPile, Turn } from '../types';
 import {
-  coinFlip,
   isGameStart,
-  flippableLocation,
-  nothingToFlip,
+  flippableLocation, // only give column as argument 🤯
   randomIndex,
+  findLocationToClearColumn,
 } from '../utils/bot-helpers';
 
 const COLLECT2YEET = 3;
@@ -27,13 +26,37 @@ export const r3m00nBot = (
   discardPile: DiscardPile
 ): Turn => {
   // flip random cards at game start
-  if (isGameStart(yourBoard)) {
-    // flip top-left
-    if (yourBoard[0][0] === undefined) {
+
+  handleGameStart(yourBoard);
+
+  // not yet drawn
+  if (!drawCard) {
+    // if card is bad
+    if (discardCard >= COLLECT2YEET) {
+    } else {
+      // if discard card is good
+      // guck ob man spalte wegmachen kann und wie viele Punkte dadurch gutgemacht werden würden
+      // suche aufbewahrspalte & place wenn dadurch keine Minuskarten weggehen
+    }
+
+    return { action: 'draw' };
+  } else {
+  }
+
+  return {
+    action: 'takeFromDrawPile',
+    location: [randomIndex(yourBoard.length), randomIndex(3)],
+  };
+};
+
+const handleGameStart = (board: Board): Turn | void => {
+  if (isGameStart(board)) {
+    if (board[0][0] === undefined) {
+      // flip top-left first
       return { action: 'flip', location: [0, 0] };
     }
 
-    if (yourBoard[0][0] >= COLLECT2YEET) {
+    if (board[0][0] >= COLLECT2YEET) {
       // if goal is to collect card, flip below
       return { action: 'flip', location: [0, 1] };
     } else {
@@ -41,31 +64,13 @@ export const r3m00nBot = (
       return { action: 'flip', location: [1, 0] };
     }
   }
+};
 
-  // not yet drawn
-  if (!drawCard) {
-    if (coinFlip()) {
-      // 50% chance to draw
-      return { action: 'draw' };
-    } else {
-      // 50% chance to take from discard pile
-      return {
-        action: 'takeFromDiscardPile',
-        location: [randomIndex(yourBoard.length), randomIndex(3)],
-      };
-    }
-  } else {
-    // already drawn
-    if (coinFlip() || nothingToFlip(yourBoard)) {
-      // 50% chance to take from draw pile
-      // also take from draw pile if nothing to flip
-      return {
-        action: 'takeFromDrawPile',
-        location: [randomIndex(yourBoard.length), randomIndex(3)],
-      };
-    } else {
-      // 50% chance to flip
-      return { action: 'flip', location: flippableLocation(yourBoard) };
-    }
-  }
+export const clearColumnIfPossible = (
+  board: Board,
+  card: number
+): Turn | void => {
+  const location = findLocationToClearColumn(board, card);
+  if (location) console.log(location);
+  if (location) return { action: 'takeFromDiscardPile', location };
 };
